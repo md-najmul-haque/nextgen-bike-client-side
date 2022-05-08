d44cb1b77e5b2d55dd38c0e410783c352e0c9fa9
najuml-bd <najmul0612@gmail.com>
2022-05-08 18:25:47 +0600

    hooks added

---
+++ src/hooks/useInventories.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+
+const useInventories = () => {
+    const [inventories, setInventories] = useState([]);
+    useEffect(() => {
+        fetch('inventories.json')
+            .then(res => res.json())
+            .then(data => setInventories(data))
+    }, [])
+
+
+    return [inventories, setInventories];
+};
+
+export default useInventories;
