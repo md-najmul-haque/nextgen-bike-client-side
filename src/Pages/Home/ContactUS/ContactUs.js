af533ee11efa09dbd726464a95af29600407cac3
najuml-bd <najmul0612@gmail.com>
2022-05-10 16:41:03 +0600

    contact us complete

--- src/Pages/Home/ContactUS/ContactUs.js
+++ src/Pages/Home/ContactUS/ContactUs.js
@@ -1,100 +1,106 @@
 import React from 'react';
-import { Button, Col, Form, Row } from 'react-bootstrap';
+import { Col, Form, Row } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faGears, faPersonBiking, faTruckFast, faUsersGear } from '@fortawesome/free-solid-svg-icons'
 import './ContactUs.css'
 
 const ContactUs = () => {
     return (
-        <div className='d-flex justify-content-center alien-item-center'>
-            <div className='w-75 mx-auto p-4'>
-                <div>
-                    <h3>WHY SHOULD YOU CONSIDER US?</h3>
-                    <p>You might be surprised how awesome service we provide for your needs.</p>
+
+        <div className='contact-container'>
+            <h1 className='section-title'>WHY SHOULD YOU CONSIDER US?</h1>
+            <div className='d-flex justify-content-center alien-item-center flex-column flex-lg-row'>
+                <div className='w-75 mx-auto p-4 service-container container'>
+                    <div>
+                        <h3 className='section-sub-title'>OUR SERVICES</h3>
+                        <p>You might be surprised how awesome service we provide for your needs.</p>
+                    </div>
+                    <div className='service-section'>
+                        <div>
+                            <p> <span><FontAwesomeIcon icon={faPersonBiking} /></span> <span>Innovative Designs</span></p>
+                            <p>We provide innovative designs of bicycle which is most trending and fashionable nowadays</p>
+                        </div>
+                        <div>
+                            <p> <span><FontAwesomeIcon icon={faGears} /></span> <span>Long Lasting</span></p>
+                            <p>Our designed designs bicycle will be more long-lasting than other conventional designs.</p>
+                        </div>
+                        <div>
+                            <p> <span><FontAwesomeIcon icon={faTruckFast} /></span> <span>Quick Shipment</span></p>
+                            <p>We delivered your order in the shortest time anywhere in the country</p>
+                        </div>
+                        <div>
+                            <p> <span><FontAwesomeIcon icon={faUsersGear} /></span> <span>24/7 customer service</span></p>
+                            <p>We Provide 24/7 customer service to give you the best service</p>
+                        </div>
+                    </div>
+
                 </div>
-                <div className='service-section'>
+                <div className='w-75 mx-auto p-4'>
+                    <h3 className='contact-us-heading'>Contact Us</h3>
+
                     <div>
-                        <p>Innovative Designs</p>
-                        <p>We provide innovative designs of bicycle which is most trending and fashionable nowadays</p>
+                        <Form className='contact-form'>
+                            <Row className="mb-3 text-start">
+                                <Form.Group as={Col} controlId="formGridEmail">
+
+                                    <Form.Control type="text" placeholder="Your Name" />
+                                </Form.Group>
+
+                                <Form.Group as={Col} controlId="formGridPassword">
+
+                                    <Form.Control type="text" placeholder="Your Phone Number" />
+                                </Form.Group>
+                            </Row>
+                            <Row className="mb-3 text-start">
+                                <Form.Group as={Col} controlId="formGridEmail">
+
+                                    <Form.Control type="email" placeholder="Enter email" />
+                                </Form.Group>
+
+                                <Form.Group as={Col} controlId="formGridPassword">
+
+                                    <Form.Control type="password" placeholder="Password" />
+                                </Form.Group>
+                            </Row>
+
+                            <Form.Group className="mb-3 text-start" controlId="formGridAddress1">
+
+                                <Form.Control placeholder="1234 Main St, Apartment" />
+                            </Form.Group>
+
+                            <Row className="mb-3 text-start">
+                                <Form.Group as={Col} controlId="formGridCity">
+                                    <Form.Control placeholder="City" />
+                                </Form.Group>
+
+                                <Form.Group as={Col} controlId="formGridCity">
+                                    <Form.Control placeholder="State" />
+                                </Form.Group>
+
+                                <Form.Group as={Col} controlId="formGridZip">
+                                    <Form.Control placeholder="Zip" />
+                                </Form.Group>
+                            </Row>
+
+
+                            <Form.Group className="mb-3 text-start" controlId="formGridAddress1">
+
+                                <Form.Control placeholder="Leave your message or what you want to buy from us?" />
+                            </Form.Group>
+
+                            <Form.Group className="mb-3 text-start" id="formGridCheckbox">
+                                <Form.Check type="checkbox" label="Please send me your company news letter also." />
+                            </Form.Group>
+
+                            <button className='button-style mb-0' type="submit">
+                                Submit
+                            </button>
+                        </Form>
                     </div>
-                    <div>
-                        <p>Long Lasting</p>
-                        <p>Our designed designs bicycle will be more long-lasting than other conventional designs.</p>
-                    </div>
-                    <div>
-                        <p>Quick Shipment</p>
-                        <p>We delivered your order in the shortest time anywhere in the country</p>
-                    </div>
-                    <div>
-                        <p>24/7 customer service</p>
-                        <p>We Provide 24/7 customer service to give you the best service</p>
-                    </div>
+
                 </div>
 
             </div>
-            <div className='w-75 p-4'>
-                <h3 className='contact-us-heading'>Contact Us</h3>
-
-                <div>
-                    <Form className='contact-form'>
-                        <Row className="mb-3 text-start">
-                            <Form.Group as={Col} controlId="formGridEmail">
-
-                                <Form.Control type="text" placeholder="Your Name" />
-                            </Form.Group>
-
-                            <Form.Group as={Col} controlId="formGridPassword">
-
-                                <Form.Control type="text" placeholder="Your Phone Number" />
-                            </Form.Group>
-                        </Row>
-                        <Row className="mb-3 text-start">
-                            <Form.Group as={Col} controlId="formGridEmail">
-
-                                <Form.Control type="email" placeholder="Enter email" />
-                            </Form.Group>
-
-                            <Form.Group as={Col} controlId="formGridPassword">
-
-                                <Form.Control type="password" placeholder="Password" />
-                            </Form.Group>
-                        </Row>
-
-                        <Form.Group className="mb-3 text-start" controlId="formGridAddress1">
-
-                            <Form.Control placeholder="1234 Main St, Apartment" />
-                        </Form.Group>
-
-                        <Row className="mb-3 text-start">
-                            <Form.Group as={Col} controlId="formGridCity">
-                                <Form.Control placeholder="City" />
-                            </Form.Group>
-
-                            <Form.Group as={Col} controlId="formGridCity">
-                                <Form.Control placeholder="State" />
-                            </Form.Group>
-
-                            <Form.Group as={Col} controlId="formGridZip">
-                                <Form.Control placeholder="Zip" />
-                            </Form.Group>
-                        </Row>
-
-
-                        <Form.Group className="mb-3 text-start" controlId="formGridAddress1">
-
-                            <Form.Control placeholder="Leave your message or what you want to buy from us?" />
-                        </Form.Group>
-
-                        <Form.Group className="mb-3 text-start" id="formGridCheckbox">
-                            <Form.Check type="checkbox" label="Please send me your company news letter also." />
-                        </Form.Group>
-
-                        <button className='button-style mb-0' type="submit">
-                            Submit
-                        </button>
-                    </Form>
-                </div>
-
-            </div>
-
         </div>
     );
 };
---
+++ src/Pages/Home/Feature/Feature.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Feature = () => {
+    return (
+        <div>
+            <div><h1>FEATURES</h1></div>
+        </div>
+    );
+};
+
+export default Feature;
